196a9a27cf6011c767aa0f9c49fc9f5fe75bde4e
hal1008 <dev.share@plaid.co.jp>
2023-08-10 09:28:55 +0000

    [android] Add visualtracking 2.9.0 documents

---
+++ docs/android/visualtracking/2.9.0/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":visualtracking:dokkaHtml/androidTestRelease": [], ":visualtracking:dokkaHtml/debug": [], ":visualtracking:dokkaHtml/main": [], ":visualtracking:dokkaHtml/release": []}'
