cd5bb94a655da458c0c13083920bf3fe3f4fecd4
hal1008 <dev.share@plaid.co.jp>
2023-08-10 09:28:53 +0000

    [android] Add inappmessaging 2.16.0 documents

---
+++ docs/android/inappmessaging/2.16.0/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":inappmessaging:dokkaHtml/androidTestRelease": [], ":inappmessaging:dokkaHtml/debug": [], ":inappmessaging:dokkaHtml/main": [], ":inappmessaging:dokkaHtml/release": []}'
